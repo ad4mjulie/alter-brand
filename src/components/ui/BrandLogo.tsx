c17e37912f8b88a773a00c9a45ed8da0163ee554
adam <ad4msjullie@gmail.com>
2026-01-24 02:10:14 +0100

    design: update brand logo to AKHER3OUD.png

--- src/components/ui/BrandLogo.tsx
+++ src/components/ui/BrandLogo.tsx
@@ -5,7 +5,7 @@ export default function BrandLogo({ className }: { className?: string }) {
     return (
         <div className={clsx("relative h-32 w-64 transition-colors z-50", className)}>
             <Image
-                src="/brand-logo-v2.png"
+                src="/akher3oud.png"
                 alt="ALTER Logo"
                 fill
                 className="object-contain"
